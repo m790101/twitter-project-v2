fd7313dc51ae4de60de9af6b03cd3a6c9625bde7
m790101 <m790101@hotmail.com>
2023-02-11 18:38:13 +0800

    change base_url for API

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'https://twitter-api-2022-10.herokuapp.com/api'
+const baseURL = 'https://twitter-2022.onrender.com/api'
 
 const axiosInstance = axios.create({
   baseURL
